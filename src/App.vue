59c6c8fbc6088cd69b14279756f1704721ab59df
Liyaan66 <liya.an@student.passerellesnumeriques.org>
2025-06-13 20:18:40 +0700

    props and emit vue with f-delete

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container max-auto p-5">
+    <List v-for="(user, index) in users" 
+  :key="user.id" 
+  :user="user"
+  :index="index"
+ @deleteUser="removeUser"/>
+  </div>
+</template>
+
+
+<script setup>
+import { reactive, ref} from 'vue'
+import List from './components/List.vue';
+
+const removeUser = (index) => {
+  users.splice(index, 1)
+}
+
+const users = reactive ([
+  { id: 1, name: "John Doe", age: 30, position: "Software Engineer" },
+  { id: 2, name: "Jane Smith", age: 25, position: "Product Manager" },
+  { id: 3, name: "Alice Johnson", age: 28, position: "UX Designer" },
+  { id: 4, name: "Bob Brown", age: 35, position: "Data Scientist" },
+]);
+</script>
---
+++ src/components/List.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul class="space-y-4 p-4 bg-gray-50 rounded-xl shadow-md">
+    <li class="flex items-start justify-between bg-purple-50 p-4 rounded-xl hover:bg-purple-100 transition duration-200">
+      <div>
+        <p class="text-sm font-semibold text-purple-600">ID: {{ user.id }}</p>
+        <p class="text-lg font-medium text-gray-800">{{ user.name }}</p>
+        <p class="text-sm text-gray-600">{{ user.age }} years old</p>
+      </div>
+    </li>
+    <button @click="$emit('deleteUser', index)">delete</button>
+  </ul>
+</template>
+
+
+
+<script setup>
+
+defineProps({
+  user: {
+  type: Object,
+  required: true
+  },
+  index: {
+  type: Number,
+  required: true
+  }
+
+
+})
+</script>
